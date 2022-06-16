47bbdcbc19ef0a92bd67d3d1ad6302552996f332
Bordunos <Mr.bordunos@mail.ru>
2022-06-16 16:06:51 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication20.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <fstream>
+#include "car.pb.h"
+using namespace std;
+int main() {
+	Car car;
+	car.set_brand("Toyota");
+	car.set_model("Camry");
+	car.set_year(2011);
+	cout << car.brand() << " " << car.model() << " " << car.year() << endl;
+	ofstream out("/Users/semyon/Desktop/protobuf_lesson/car.bin", ios_base::binary);
+	car.SerializeToOstream(&out);
+}
